ba148b2aa9e216d6541571005eec272d1412410a
Vinicius Oliveira <vinicius.marchesini@live.com>
2022-02-11 05:44:47 -0300

    SellerFormViewModel and Department select Component

---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+
+namespace SalesWebMvc.Models
+{
+    public class SellerFormViewModel
+    {
+        public Seller Seller { get; set; }
+        public ICollection<Department> Departments { get; set; }
+    }
+}
